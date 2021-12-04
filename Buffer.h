17a46cc19f2151cd709192a5c31d1cfe6460d309
Apourtartt <ApourtArtt@gmail.com>
2021-12-04 16:48:58 +0100

    Added class bufferization

--- Buffer.h
+++ Buffer.h
@@ -2,102 +2,169 @@
 #include <bit> // C++20, endianness
 #include <string>
 
-template<typename T>
-T ParseOneByte(const char* data, int offset)
+namespace Buffer
 {
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    return static_cast<T>(*(d + offset));
-}
-
-template<typename T>
-T ParseTwoBytes(const char* data, int offset)
-{
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    if constexpr (std::endian::native == std::endian::big)
+    template<typename T>
+    T ParseOneByte(const char* data, int offset)
     {
-        return static_cast<T>(
-            (*(d + offset) << 8) |
-            (*(d + offset + 1)));
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
+        return static_cast<T>(*(d + offset));
     }
-    else if constexpr (std::endian::native == std::endian::little)
+
+    template<typename T>
+    T ParseTwoBytes(const char* data, int offset)
     {
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
+        if constexpr (std::endian::native == std::endian::big)
+            return ParseTwoBytesBigEndian<T>(data, offset);
+        else if constexpr (std::endian::native == std::endian::little)
+            return ParseTwoBytesLittleEndian<T>(data, offset);
+    }
+
+    template<typename T>
+    T ParseTwoBytesLittleEndian(const char* data, int offset)
+    {
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
         return static_cast<T>(
             (*(d + offset + 1) << 8) |
             (*(d + offset)));
     }
-}
 
-template<typename T>
-T ParseTwoBytesLittleEndian(const char* data, int offset)
-{
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    return static_cast<T>(
-        (*(d + offset + 1) << 8) |
-        (*(d + offset)));
-}
-
-template<typename T>
-T ParseTwoBytesBigEndian(const char* data, int offset)
-{
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    return static_cast<T>(
-        (*(d + offset) << 8) |
-        (*(d + offset + 1)));
-}
-
-template<typename T>
-T ParseFourBytes(const char* data, int offset)
-{
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    if constexpr (std::endian::native == std::endian::big)
+    template<typename T>
+    T ParseTwoBytesBigEndian(const char* data, int offset)
     {
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
         return static_cast<T>(
-            (*(d + offset) << 24) |
-            (*(d + offset + 1) << 16) |
-            (*(d + offset + 2) << 8) |
-            (*(d + offset + 3)));
+            (*(d + offset) << 8) |
+            (*(d + offset + 1)));
     }
-    else if constexpr (std::endian::native == std::endian::little)
+
+    template<typename T>
+    T ParseFourBytes(const char* data, int offset)
     {
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
+        if constexpr (std::endian::native == std::endian::big)
+            return ParseFourBytesBigEndian<T>(data, offset);
+        else if constexpr (std::endian::native == std::endian::little)
+            return ParseFourBytesLittleEndian<T>(data, offset);
+    }
+
+    template<typename T>
+    T ParseFourBytesLittleEndian(const char* data, int offset)
+    {
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
         return static_cast<T>(
             (*(d + offset + 3) << 24) |
             (*(d + offset + 2) << 16) |
             (*(d + offset + 1) << 8) |
             (*(d + offset)));
     }
-}
 
-template<typename T>
-T ParseFourBytesLittleEndian(const char* data, int offset)
-{
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    return static_cast<T>(
-        (*(d + offset + 3) << 24) |
-        (*(d + offset + 2) << 16) |
-        (*(d + offset + 1) << 8) |
-        (*(d + offset)));
-}
+    template<typename T>
+    T ParseFourBytesBigEndian(const char* data, int offset)
+    {
+        const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
+        return static_cast<T>(
+            (*(d + offset) << 24) |
+            (*(d + offset + 1) << 16) |
+            (*(d + offset + 2) << 8) |
+            (*(d + offset + 3)));
+    }
 
-template<typename T>
-T ParseFourBytesBigEndian(const char* data, int offset)
-{
-    const unsigned char* d = reinterpret_cast<const unsigned char*>(data);
-    return static_cast<T>(
-        (*(d + offset) << 24) |
-        (*(d + offset + 1) << 16) |
-        (*(d + offset + 2) << 8) |
-        (*(d + offset + 3)));
-}
+    template<typename T>
+    char* ParseOneByte(T data)
+    {
+        return reinterpret_cast<char*>(data);
+    }
 
-std::string ParseString(const char* data, int offset)
-{
-    std::string output;
-    for (int i = 0; *(data + offset + i); i++)
-        output += *(data + offset + i);
-    return std::move(output);
-}
+    template<typename T>
+    char* ParseTwoBytes(T data)
+    {
+        if constexpr (std::endian::native == std::endian::big)
+            return ParseTwoBytesBigEndian<T>(data);
+        else if constexpr (std::endian::native == std::endian::little)
+            return ParseTwoBytesLittleEndian<T>(data);
+    }
 
-std::string ParseStringWithSize(const char* data, int offset, int size)
-{
-    return std::move(std::string(data + offset, size));
+    template<typename T>
+    char* ParseTwoBytesLittleEndian(T data)
+    {
+        char* res = (char*)malloc(4);
+        res[0] = (data >> 8 & 0xFF);
+        res[1] = (data & 0xFF);
+        return res;
+    }
+
+    template<typename T>
+    char* ParseTwoBytesBigEndian(T data)
+    {
+        char* res = (char*)malloc(4);
+        res[0] = (data & 0xFF);
+        res[1] = (data >> 8 & 0xFF);
+        return res;
+    }
+
+    template<typename T>
+    char* ParseFourBytes(T data)
+    {
+        if constexpr (std::endian::native == std::endian::big)
+            return ParseFourBytesBigEndian<T>(data);
+        else if constexpr (std::endian::native == std::endian::little)
+            return ParseFourBytesLittleEndian<T>(data);
+    }
+
+    template<typename T>
+    char* ParseFourBytesLittleEndian(T data)
+    {
+        char* res = (char*)malloc(4);
+        res[0] = (data & 0xFF);
+        res[1] = (data >> 8 & 0xFF);
+        res[2] = (data >> 16 & 0xFF);
+        res[3] = (data >> 24 & 0xFF);
+        return res;
+    }
+
+    template<typename T>
+    char* ParseFourBytesBigEndian(T data)
+    {
+        char* res = (char*)malloc(4);
+        res[0] = (data >> 24 & 0xFF);
+        res[1] = (data >> 16 & 0xFF);
+        res[2] = (data >> 8 & 0xFF);
+        res[3] = (data & 0xFF);
+        return res;
+    }
+
+    std::string ParseString(const char* data, int offset)
+    {
+        return std::move(std::string(data + offset));
+    }
+
+    std::string ParseStringWithSize(const char* data, int offset, int size)
+    {
+        return std::move(std::string(data + offset, size));
+    }
+
+    char* InitializeBuffer(int size)
+    {
+        if (size <= 0)
+            return nullptr;
+        char* buffer = (char*)malloc(size);
+        if (buffer == nullptr)
+            return nullptr;
+        memset(buffer, 0, size);
+        return buffer;
+    }
+
+    void Concat(char* prefix, const char* suffix, int size)
+    {
+        memcpy(prefix, suffix, size);
+    }
+
+    class Bufferizable
+    {
+    public:
+        virtual char* Bufferize() { return nullptr; }
+        virtual void Unbufferize(const char* Buffer) {}
+    };
 }
--- main.cpp
+++ main.cpp
@@ -1,102 +1,142 @@
 #include "Buffer.h"
 #include "Tester.h"
 
-TEST("ParseOneByte", "work",
+TEST("Buffer::ParseOneByte", "work",
 	char* buffer = new char[256];
 
-	int index = 0;
-	for (char i = -128; i < 127; i++)
-	{
-		buffer[index] = i;
-		EXPECT("ParseOneByte(" + std::to_string(i) + ") to be " + std::to_string(i), ParseOneByte<unsigned char>(buffer, index) == static_cast<unsigned char>(i));
-		index++;
-	}
+int index = 0;
+for (char i = -128; i < 127; i++)
+{
+	buffer[index] = i;
+	EXPECT("Buffer::ParseOneByte(" + std::to_string(i) + ") to be " + std::to_string(i), Buffer::ParseOneByte<unsigned char>(buffer, index) == static_cast<unsigned char>(i));
+	index++;
+}
 );
 
-BENCH("ParseOneByte", "be fast",
+BENCH("Buffer::ParseOneByte", "be fast",
 	const char* buffer = "\x25\x2f";
 
 START_BENCH;
-	ParseOneByte<unsigned char>(buffer, 1);
+Buffer::ParseOneByte<unsigned char>(buffer, 1);
 STOP_BENCH;
 );
 
-TEST("ParseTwoBytes", "work",
+TEST("Buffer::ParseTwoBytes", "work",
 	const char* buffer = "\x16\x25\x2f\xff";
 
-EXPECT("ParseTwoBytes(\"\\x16\\x25\") to be 9494", ParseTwoBytes<short>(buffer, 0) == 9494);
-EXPECT("ParseTwoBytesLittleEndian(\"\\x16\\x25\") to be 9494", ParseTwoBytesLittleEndian<short>(buffer, 0) == 9494);
-EXPECT("ParseTwoBytesBigEndian(\"\\x16\\x25\") to be 5669", ParseTwoBytesBigEndian<short>(buffer, 0) == 5669);
+EXPECT("Buffer::ParseTwoBytes(\"\\x16\\x25\") to be 9494", Buffer::ParseTwoBytes<short>(buffer, 0) == 9494);
+EXPECT("Buffer::ParseTwoBytesLittleEndian(\"\\x16\\x25\") to be 9494", Buffer::ParseTwoBytesLittleEndian<short>(buffer, 0) == 9494);
+EXPECT("Buffer::ParseTwoBytesBigEndian(\"\\x16\\x25\") to be 5669", Buffer::ParseTwoBytesBigEndian<short>(buffer, 0) == 5669);
 
-EXPECT("ParseTwoBytes(\"\\x25\\x2F\") to be 12069", ParseTwoBytes<short>(buffer, 1) == 12069);
-EXPECT("ParseTwoBytesLittleEndian(\"\\x25\\x2F\") to be 12069", ParseTwoBytesLittleEndian<short>(buffer, 1) == 12069);
-EXPECT("ParseTwoBytesBigEndian(\"\\x25\\x2F\") to be 9519", ParseTwoBytesBigEndian<short>(buffer, 1) == 9519);
+EXPECT("Buffer::ParseTwoBytes(\"\\x25\\x2F\") to be 12069", Buffer::ParseTwoBytes<short>(buffer, 1) == 12069);
+EXPECT("Buffer::ParseTwoBytesLittleEndian(\"\\x25\\x2F\") to be 12069", Buffer::ParseTwoBytesLittleEndian<short>(buffer, 1) == 12069);
+EXPECT("Buffer::ParseTwoBytesBigEndian(\"\\x25\\x2F\") to be 9519", Buffer::ParseTwoBytesBigEndian<short>(buffer, 1) == 9519);
 
-EXPECT("ParseTwoBytes(\"\\x2F\\xFF\") to be 65327", ParseTwoBytes<unsigned short>(buffer, 2) == 65327);
-EXPECT("ParseTwoBytesLittleEndian(\"\\x2F\\xFF\") to be 65327", ParseTwoBytesLittleEndian<unsigned short>(buffer, 2) == 65327);
-EXPECT("ParseTwoBytes(\"\\x2F\\xFF\") to be -209", ParseTwoBytes<short>(buffer, 2) == -209);
-EXPECT("ParseTwoBytesLittleEndian(\"\\x2F\\xFF\") to be -209", ParseTwoBytesLittleEndian<short>(buffer, 2) == -209);
-EXPECT("ParseTwoBytesBigEndian(\"\\x2F\\xFF\") to be 12287", ParseTwoBytesBigEndian<short>(buffer, 2) == 12287);
+EXPECT("Buffer::ParseTwoBytes(\"\\x2F\\xFF\") to be 65327", Buffer::ParseTwoBytes<unsigned short>(buffer, 2) == 65327);
+EXPECT("Buffer::ParseTwoBytesLittleEndian(\"\\x2F\\xFF\") to be 65327", Buffer::ParseTwoBytesLittleEndian<unsigned short>(buffer, 2) == 65327);
+EXPECT("Buffer::ParseTwoBytes(\"\\x2F\\xFF\") to be -209", Buffer::ParseTwoBytes<short>(buffer, 2) == -209);
+EXPECT("Buffer::ParseTwoBytesLittleEndian(\"\\x2F\\xFF\") to be -209", Buffer::ParseTwoBytesLittleEndian<short>(buffer, 2) == -209);
+EXPECT("Buffer::ParseTwoBytesBigEndian(\"\\x2F\\xFF\") to be 12287", Buffer::ParseTwoBytesBigEndian<short>(buffer, 2) == 12287);
 );
 
-BENCH("ParseTwoBytes", "be fast",
-	const char* buffer = "\x16\x25\x2f\xff";
-
-START_BENCH;
-	ParseTwoBytes<short>(buffer, 1);
-STOP_BENCH;
-);
-
-TEST("ParseFourBytes", "work",
-	const char* buffer = "\x16\x25\x2f\xff";
-
-EXPECT("ParseFourBytes(\"\\x16\\x25\\x2F\\xFF\") to be 4281279766", ParseFourBytes<int>(buffer, 0) == -13687530);
-EXPECT("ParseFourBytesLittleEndian(\"\\x16\\x25\\x2F\\xFF\") to be 4281279766", ParseFourBytesLittleEndian<unsigned int>(buffer, 0) == 4281279766);
-EXPECT("ParseFourBytesBigEndian(\"\\x16\\x25\\x2F\\xFF\") to be 371535871", ParseFourBytesBigEndian<int>(buffer, 0) == 371535871);
-);
-
-BENCH("ParseFourBytes", "be fast",
+BENCH("Buffer::ParseTwoBytes", "be fast",
 	const char* buffer = "\x16\x25\x2f\xff";
 
 START_BENCH;
-ParseFourBytes<int>(buffer, 0);
+Buffer::ParseTwoBytes<short>(buffer, 1);
 STOP_BENCH;
 );
 
-TEST("ParseString", "work",
-	const char* buffer = "hello\0world";
+TEST("Buffer::ParseFourBytes", "work",
+	const char* buffer = "\x16\x25\x2f\xff";
 
-EXPECT("ParseString(\"hello\\0world\", 0) to be \"hello\"", ParseString(buffer, 0) == "hello");
-EXPECT("ParseString(\"hello\\0world\", 1) to be \"ello\"", ParseString(buffer, 1) == "ello");
-EXPECT("ParseString(\"hello\\0world\", 5) to be \"\"", ParseString(buffer, 5) == "\x00");
-EXPECT("ParseString(\"hello\\0world\", 6) to be \"world\"", ParseString(buffer, 6) == "world");
+EXPECT("Buffer::ParseFourBytes(\"\\x16\\x25\\x2F\\xFF\") to be 4281279766", Buffer::ParseFourBytes<int>(buffer, 0) == -13687530);
+EXPECT("Buffer::ParseFourBytesLittleEndian(\"\\x16\\x25\\x2F\\xFF\") to be 4281279766", Buffer::ParseFourBytesLittleEndian<unsigned int>(buffer, 0) == 4281279766);
+EXPECT("Buffer::ParseFourBytesBigEndian(\"\\x16\\x25\\x2F\\xFF\") to be 371535871", Buffer::ParseFourBytesBigEndian<int>(buffer, 0) == 371535871);
 );
 
-BENCH("ParseString   ", "be fast",
+BENCH("Buffer::ParseFourBytes", "be fast",
+	const char* buffer = "\x16\x25\x2f\xff";
+
+START_BENCH;
+Buffer::ParseFourBytes<int>(buffer, 0);
+STOP_BENCH;
+);
+
+TEST("Buffer::ParseString", "work",
+	const char* buffer = "hello\0world";
+
+EXPECT("Buffer::ParseString(\"hello\\0world\", 0) to be \"hello\"", Buffer::ParseString(buffer, 0) == "hello");
+EXPECT("Buffer::ParseString(\"hello\\0world\", 1) to be \"ello\"", Buffer::ParseString(buffer, 1) == "ello");
+EXPECT("Buffer::ParseString(\"hello\\0world\", 5) to be \"\"", Buffer::ParseString(buffer, 5) == "\x00");
+EXPECT("Buffer::ParseString(\"hello\\0world\", 6) to be \"world\"", Buffer::ParseString(buffer, 6) == "world");
+);
+
+BENCH("Buffer::ParseString   ", "be fast",
 	const char* buffer = "hello\0world";
 
 START_BENCH;
-ParseString(buffer, 0);
+Buffer::ParseString(buffer, 0);
 STOP_BENCH;
 );
 
-TEST("ParseStringWithSize", "work",
+TEST("Buffer::ParseStringWithSize", "work",
 	const char* buffer = "hello\0world";
 
-EXPECT("ParseStringWithSize(\"hello\\0world\", 0, 11) to be \"hello\\0world\"", ParseStringWithSize(buffer, 0, 11) == std::string("hello\0world", 11));
-EXPECT("ParseStringWithSize(\"hello\\0world\", 1, 10) to be \"ello\"", ParseStringWithSize(buffer, 1, 10) == std::string("ello\0world", 10));
-EXPECT("ParseStringWithSize(\"hello\\0world\", 5, 1) to be \"\"", ParseStringWithSize(buffer, 5, 1) == std::string("\0", 1));
-EXPECT("ParseStringWithSize(\"hello\\0world\", 6, 5) to be \"world\"", ParseStringWithSize(buffer, 6, 5) == "world");
-EXPECT("ParseStringWithSize(\"hello\\0world\", 6, 5) to be \"world\"", ParseStringWithSize(buffer, 1, 0) == "");
+EXPECT("Buffer::ParseStringWithSize(\"hello\\0world\", 0, 11) to be \"hello\\0world\"", Buffer::ParseStringWithSize(buffer, 0, 11) == std::string("hello\0world", 11));
+EXPECT("Buffer::ParseStringWithSize(\"hello\\0world\", 1, 10) to be \"ello\"", Buffer::ParseStringWithSize(buffer, 1, 10) == std::string("ello\0world", 10));
+EXPECT("Buffer::ParseStringWithSize(\"hello\\0world\", 5, 1) to be \"\"", Buffer::ParseStringWithSize(buffer, 5, 1) == std::string("\0", 1));
+EXPECT("Buffer::ParseStringWithSize(\"hello\\0world\", 6, 5) to be \"world\"", Buffer::ParseStringWithSize(buffer, 6, 5) == "world");
+EXPECT("Buffer::ParseStringWithSize(\"hello\\0world\", 6, 5) to be \"world\"", Buffer::ParseStringWithSize(buffer, 1, 0) == "");
 );
 
-BENCH("ParseStringWithSize", "be fast",
+BENCH("Buffer::ParseStringWithSize", "be fast",
 	const char* buffer = "hello\0world";
 
 START_BENCH;
-ParseStringWithSize(buffer, 0, 11);
+Buffer::ParseStringWithSize(buffer, 0, 11);
 STOP_BENCH;
 );
+
+class Dummy : public Buffer::Bufferizable
+{
+public:
+	Dummy(int A, int B, std::string C) : a(A), b(B), c(C) {}
+
+	char* Bufferize() override
+	{
+		char* buffer = Buffer::InitializeBuffer(sizeof a + sizeof b + 1 + c.size());
+		auto t = Buffer::ParseFourBytesLittleEndian(b);
+		Buffer::Concat(buffer, Buffer::ParseFourBytesLittleEndian(a), 4);
+		Buffer::Concat(buffer + 4, Buffer::ParseFourBytesLittleEndian(b), 4);
+		Buffer::Concat(buffer + 8, Buffer::ParseFourBytesLittleEndian(c.size()), 4);
+		Buffer::Concat(buffer + 12, c.c_str(), c.size());
+		return buffer;
+	}
+
+	void Unbufferize(const char* Buffer) override
+	{
+		a = Buffer::ParseFourBytesLittleEndian<int>(Buffer, 0);
+		b = Buffer::ParseFourBytesLittleEndian<int>(Buffer, 4);
+		auto size = Buffer::ParseFourBytesLittleEndian<int>(Buffer, 8);
+		c = Buffer::ParseStringWithSize(Buffer, 12, size);
+	}
+
+	int a;
+	int b;
+	std::string c;
+};
+
+TEST("Bufferizable::Bufferize", "Handle simple class",
+	Dummy dummy(0, 0, ""); // 0x5 0x1e240 hello
+const char* buffer = "\x5\x00\x00\x00\x40\xe2\x01\x00\x6\x00\x00\x00hellob";
+dummy.Unbufferize(buffer);
+EXPECT("a to be 5", dummy.a == 5);
+EXPECT("b to be 123456", dummy.b == 123456);
+EXPECT("c to be \"hellob\"", dummy.c == "hellob");
+const char* new_buffer = dummy.Bufferize();
+EXPECT("buffer and new_buffer to be identical", memcmp(buffer, new_buffer, 18) == 0);
+)
 
 int main()
 {
